9554184b4353d7d1fb585f7eb7fae302d5c406f8
aalberti <aa@antoinealberti.com>
2018-08-06 11:11:27 +0200

    2-book sets

--- src/main/kotlin/Potter.kt
+++ src/main/kotlin/Potter.kt
@@ -1,1 +1,18 @@
-fun price(vararg books: Any): Double = 8.0 * books.size
+fun price(books: Collection<Any>): Double {
+    val twoBookSet = books.twoBookSet()
+    return if (twoBookSet != null)
+        15.2 + price(books.minusAll(twoBookSet))
+    else
+        8.0 * books.size
+}
+
+private fun Collection<Any>.twoBookSet(): Collection<Any>? {
+    val distinct = distinct()
+    return if (distinct.size < 2) null
+    else distinct.take(2)
+}
+
+fun Collection<Any>.minusAll(toRemove:Collection<Any>): Collection<Any> = when {
+    toRemove.isEmpty() -> this
+    else -> (this - toRemove.first()).minusAll(toRemove.drop(1))
+}
--- src/test/kotlin/PotterTest.kt
+++ src/test/kotlin/PotterTest.kt
@@ -1,14 +1,34 @@
+import io.kotlintest.forAll
 import io.kotlintest.matchers.shouldBe
 import io.kotlintest.specs.StringSpec
 
-class PotterTest: StringSpec({
+class PotterTest : StringSpec({
+    val priceForOne = 8.0
+    val priceForTwo = priceForOne * 2 * .95
+
     "no book costs 0" {
-        price() shouldBe 0.0
+        price(listOf()) shouldBe 0.0
     }
     "1 book costs 8" {
-        price(0) shouldBe 8.0
+        price(listOf(0)) shouldBe priceForOne
     }
     "1 book many times costs 8 every time" {
-        price(0, 0, 0) shouldBe 8.0 * 3
+        price(listOf(0, 0, 0)) shouldBe priceForOne * 3
+    }
+    "2-book set gets 5% discount" {
+        forAll(listOf(
+                listOf(1, 2),
+                listOf(0, 1),
+                listOf("Harry", "Potter")
+        )) {
+            price(it) shouldBe priceForTwo
+        }
+    }
+    "2-book set many times gets 5% discount every time" {
+        price(listOf(
+                1, 2,
+                1, 2,
+                1, 2
+        )) shouldBe priceForTwo * 3
     }
 })
